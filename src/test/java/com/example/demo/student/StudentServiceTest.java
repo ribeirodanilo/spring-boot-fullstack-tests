d0b1ed10a869b5d3d3b7e7ceefd0e3ea2d94260e
Danilo Ribeiro <ribeiro.danilo@gmail.com>
2022-05-13 21:36:44 -0300

    Versao Inicial

---
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -0,0 +1,117 @@
+package com.example.demo.student;
+
+import com.example.demo.student.exception.BadRequestException;
+import com.example.demo.student.exception.StudentNotFoundException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+//@ExtendWith(MockitoExtension.class)
+class StudentServiceTest {
+
+    @Mock
+    private StudentRepository studentRepository;
+    private AutoCloseable autoCloseable;
+    private StudentService underTest;
+
+    @BeforeEach
+    void setUp() {
+        autoCloseable = MockitoAnnotations.openMocks(this);
+        underTest = new StudentService(studentRepository);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        autoCloseable.close();
+    }
+
+    @Test
+    void canGetAllStudents() {
+        // when
+        underTest.getAllStudents();
+        // then
+        verify(studentRepository).findAll();
+    }
+
+    @Test
+    void canAddStudent() {
+        // given
+        Student student = new Student(
+                "Jamila",
+                "jamila@gmail.com",
+                Gender.FEMALE
+        );
+
+        // when
+        underTest.addStudent(student);
+
+        // then
+        ArgumentCaptor<Student> studentArgumentCaptor = ArgumentCaptor.forClass(Student.class);
+        verify(studentRepository).save(studentArgumentCaptor.capture());
+
+        Student capturedStudent = studentArgumentCaptor.getValue();
+        assertThat(capturedStudent).isEqualTo(student);
+    }
+
+    @Test
+    void willThrowWhenEmailIsTaken() {
+        // given
+        Student student = new Student(
+                "Jamila",
+                "jamila@gmail.com",
+                Gender.FEMALE
+        );
+        given(studentRepository.selectExistsEmail(student.getEmail())).willReturn(true);
+
+        // when
+        // then
+        assertThatThrownBy(() -> underTest.addStudent(student))
+                .isInstanceOf(BadRequestException.class)
+                .hasMessageContaining("Email " + student.getEmail() + " taken");
+        verify(studentRepository, never()).save(any());
+    }
+
+    @Test
+    void canDeleteStudent() {
+        // given
+        Long studentId = 1L;
+        given(studentRepository.existsById(studentId)).willReturn(true);
+
+        // when
+        underTest.deleteStudent(studentId);
+
+        // then
+        ArgumentCaptor<Long> studentIdArgumentCaptor = ArgumentCaptor.forClass(Long.class);
+        verify(studentRepository).deleteById(studentIdArgumentCaptor.capture());
+
+        Long capturedStudentId = studentIdArgumentCaptor.getValue();
+        assertThat(capturedStudentId).isEqualTo(studentId);
+    }
+
+    @Test
+    void willThrowWhenDeleteAnInexistentStudentByStudentId() {
+        // given
+        Long studentId = 1L;
+
+        // when
+        // then
+        assertThatThrownBy(() -> underTest.deleteStudent(studentId))
+                .isInstanceOf(StudentNotFoundException.class)
+                .hasMessageContaining("Student with id " + studentId + " does not exists");
+        verify(studentRepository, never()).deleteById(any());
+    }
+}
